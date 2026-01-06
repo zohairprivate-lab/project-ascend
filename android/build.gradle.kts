1c80fa4da4c163bca771c9460f1bc139cd12b809
zohairprivate-lab <zohairprivate@gmail.com>
2026-01-06 15:49:55 +0530

    Add mirrors to root build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,6 +2,8 @@ allprojects {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://storage.googleapis.com/download.flutter.io") }
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
